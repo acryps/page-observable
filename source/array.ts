4c49dd0cd1830d988e3a20e5e49e6f0973ca27d8
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2023-08-14 15:22:51 +0200

    Add array observer

---
+++ source/array.ts
@@ -0,0 +1,62 @@
+import { Observable } from ".";
+
+export class ObservableArray<T> extends Observable<T[]> {
+	push(item: T) {
+		this.emit([...this.currentValue, item]);
+	}
+
+	pop() {
+		const last = this.currentValue[this.currentValue.length - 1];
+		this.emit([...this.currentValue.slice(0, -1)]);
+
+		return last;
+	}
+
+	delete(item: T) {
+		const index = this.currentValue.indexOf(item);
+
+		if (index == -1) {
+			return false;
+		}
+
+		this.splice(index, 1);
+
+		return true;
+	}
+
+	deleteAny(predicate: (item: T, index: number, array: T[]) => boolean) {
+		this.emit(this.currentValue.filter((item, index) => !predicate(item, index, this.currentValue)));
+	}
+
+	find(predicate: (item: T, index: number, array: T[]) => boolean) {
+		return this.currentValue.find(predicate);
+	}
+
+	filter(predicate: (item: T, index: number, array: T[]) => boolean) {
+		return this.currentValue.filter(predicate);
+	}
+
+	some(predicate: (item: T, index: number, array: T[]) => boolean) {
+		return this.currentValue.some(predicate);
+	}
+
+	shift() {
+		const first = this.currentValue[0];
+		this.emit([...this.currentValue.slice(1)]);
+
+		return first;
+	}
+
+	unshift(item: T) {
+		this.emit([item, ...this.currentValue]);
+	}
+
+	splice(start: number, length: number, ...replacement: T[]) {
+		const copy = [...this.currentValue];
+		const deleted = copy.splice(start, length, ...replacement);
+
+		this.emit(copy);
+
+		return deleted;
+	}
+}
